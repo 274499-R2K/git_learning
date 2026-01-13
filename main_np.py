9c06f62aa3ac267c0c4552193972705355495fc8
Alessandro Razzoli <alerazzo2000@gmail.com>
2026-01-13 20:51:44 +0100

    "commitment 13/01_04"

--- main_np.py
+++ main_np.py
@@ -1,1 +1,22 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+a=np.ones(100)
+print(a,"\n","---------------------------")
+# print(f"{a.ndim=}")
+# print(f"{a.shape=}")
+# print(f"{a.dtype=}")
+
+a2=np.ones((5,5))
+# print(a2)
+
+# r=np.arange(10,100,2)
+# print(r)
+rnd=np.random.normal(0,1,1000)
+plt.scatter(np.arange(0,1000), rnd)
+# plt.show()
+
+b=a.reshape(10,10)
+print(b)
+
